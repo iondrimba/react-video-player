897cb1cbe413a99de5b25c9c52d585aa8c012bc6
Ion D. Filho <iondrimba@gmail.com>
2018-01-28 13:14:29 -0200

    extracted seeker component

---
+++ video-player/src/components/seeker/seekr.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './seeker.css';
+
+class Seeker extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  onClick(evt) {
+    this.props.onSeek((evt.nativeEvent.layerX / evt.currentTarget.offsetWidth) * 100);
+  }
+  render() {
+    return (
+      <div onClick={this.onClick.bind(this)} className="sliders">
+        <div className="bg"></div>
+        <div style={{ transform: `scaleX(${this.props.currentPosition / 100})` }} className="current"></div>
+      </div>
+    );
+  }
+}
+
+export default Seeker;

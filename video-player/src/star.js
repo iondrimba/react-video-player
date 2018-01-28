c487cc741efcde21cc463bbef0db62c1905f1819
Ion D. Filho <iondrimba@gmail.com>
2018-01-28 12:08:00 -0200

    updated star svg

--- video-player/src/star.js
+++ video-player/src/star.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Star = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox='0 0 200 200' preserveAspectRatio="xMinYMax meet">
-      <polygon points="100,10 40,198 190,78 10,78 160,198" ></polygon>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox='60 0 121 115' preserveAspectRatio="xMinYMax meet">
+     <polygon points="124.635,8.232 139.966,39.295 174.248,44.277 149.442,68.456 155.297,102.598 124.635,86.479   93.974,102.598 99.83,68.456 75.024,44.277 109.306,39.295 "/>
     </svg>
   )
 }

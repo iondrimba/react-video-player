437e91079b6448f1686c893ca31d43e8423b911f
Ion D. Filho <iondrimba@gmail.com>
2018-01-28 13:44:39 -0200

    added star icon

--- video-player/src/components/hotspot/hotSpot.js
+++ video-player/src/components/hotspot/hotSpot.js
@@ -1,18 +1,12 @@
 
 import React, { Component } from 'react';
 import './hotSpot.css';
+import Star from '../star/star';
 import convertFormatedTimeToSeconds from '../../helpers/converter';
 
 class HotSpot extends Component {
   constructor(props) {
     super(props);
-  }
-
-  componentDidMount() {
-  }
-
-  convertFormatedTimeToSeconds(time) {
-    return time.split(':').reduce((acc, time) => (60 * acc) + +time);
   }
 
   getSpotPosition(time, duration) {
@@ -24,6 +18,7 @@ class HotSpot extends Component {
   render() {
     return (
       <div className="spot" style={this.getSpotPosition(this.props.spot.time, this.props.duration)}>
+       <Star/>
         <div className="thumb">
           <img src={this.props.spot.thumb} width={200} height={150} />
           <p>{this.props.spot.description}</p>

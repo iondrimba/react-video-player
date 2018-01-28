860db8c78cfe4d4238b26a3e71bfd678462003ed
Ion D. Filho <iondrimba@gmail.com>
2018-01-28 20:53:41 -0200

    update pause button

--- video-player/src/components/playPause/index.js
+++ video-player/src/components/playPause/index.js
@@ -24,7 +24,7 @@ class PlayPause extends Component {
         </svg>
         <svg onClick={this.onPauseClick.bind(this)} className={`button ${this.props.playing ? '' : 'hide'}`} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 283.5 283.5'>
           <circle cx='141.7' cy='141.7' r='137.5' className='button__bg' />
-          <g xmlns='http://www.w3.org/2000/svg'><rect height='150' width='40' x='85' y='65'></rect><rect height='150' width='40' x='150' y='65'></rect></g>
+          <g xmlns="http://www.w3.org/2000/svg"><rect height="120" width="30" x="95" y="85"></rect><rect height="120" width="30" x="150" y="85"></rect></g>
         </svg>
       </div >
     );

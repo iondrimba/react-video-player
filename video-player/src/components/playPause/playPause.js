76286c746b1858dcec61396d15fa4cc7a3f36d33
Ion D. Filho <iondrimba@gmail.com>
2018-01-28 16:29:07 -0200

    swaped buttons for svg

--- video-player/src/components/playPause/playPause.js
+++ video-player/src/components/playPause/playPause.js
@@ -7,7 +7,6 @@ class PlayPause extends Component {
     super(props);
   }
 
-
   onPlayClick() {
     this.props.onClick(true);
   }
@@ -19,8 +18,15 @@ class PlayPause extends Component {
   render() {
     return (
       <div className="playPause"  >
-        <button onClick={this.onPlayClick.bind(this)}>play</button>
-        <button onClick={this.onPauseClick.bind(this)}>pause</button>
+        <svg onClick={this.onPlayClick.bind(this)} className={`button ${this.props.playing ? 'hide' : ''}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283.5 283.5">
+          <circle cx="141.7" cy="141.7" r="137.5" className="button__bg" />
+          <path d="M113.6 78.1L201.8 141.7 113.6 205.4" className="button__icon" />
+        </svg>
+        <svg onClick={this.onPauseClick.bind(this)} className={`button ${this.props.playing ? '' : 'hide'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 283.5 283.5">
+          <circle cx="141.7" cy="141.7" r="137.5" className="button__bg" />
+          <g xmlns="http://www.w3.org/2000/svg"><rect height="150" width="40" x="85" y="65"></rect><rect height="150" width="40" x="150" y="65"></rect></g>
+        </svg>
+
       </div >
     );
   }

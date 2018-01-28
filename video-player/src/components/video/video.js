62c463c499289672324684b8e0cdcdc937a20fcc
Ion D. Filho <iondrimba@gmail.com>
2018-01-28 19:01:43 -0200

    added transition to hotspots

--- video-player/src/components/video/video.js
+++ video-player/src/components/video/video.js
@@ -5,6 +5,7 @@ import Seeker from '../seeker/seeker';
 import HotSpot from '../hotspot/hotSpot';
 import PlayPause from '../playPause/playPause';
 import './video.css';
+import seeker from '../seeker/seeker';
 
 class Video extends Component {
   constructor(props) {
@@ -17,6 +18,7 @@ class Video extends Component {
       duarion: 0,
       currentPosition: 0,
       playing: false,
+      seeking: false
     };
 
   }
@@ -31,12 +33,24 @@ class Video extends Component {
         this.setState({ playing: false, currentPosition: 0 });
         this.history.update('/');
       }
-    })
+    });
 
     this.video.addEventListener('loadedmetadata', () => {
       this.video.volume = .1;
       this.setState({ duration: this.video.duration });
-    })
+    });
+
+    this.video.addEventListener('seeked', () => {
+      const interval = setTimeout(() => {
+        this.setState({ playing: true });
+        this.video.play();
+
+        this.seeking.classList.remove('seeking-overlay--show');
+
+        clearTimeout(interval);
+      }, 300);
+    });
+
   }
 
   seekSpot(time) {
@@ -44,7 +58,20 @@ class Video extends Component {
   }
 
   seek(percent) {
-    this.video.currentTime = (percent * this.video.duration) / 100;
+    this.setState({ playing: false });
+    this.video.pause();
+
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        this.seeking.classList.add('seeking-overlay--show');
+      });
+    });
+
+    const interval = setTimeout(() => {
+      this.video.currentTime = (percent * this.video.duration) / 100;
+      clearTimeout(interval);
+    }, 100);
+
   }
 
   onHotSpotClick(spot, evt) {
@@ -52,7 +79,6 @@ class Video extends Component {
   }
 
   onPlayPauseClick(playing) {
-
     this.setState({ playing: playing });
 
     if (this.state.playing) {
@@ -64,20 +90,22 @@ class Video extends Component {
 
   render() {
     return (
-      <div className="player">
-        <div className="video-container">
-          <video className="video" ref={(video) => { this.video = video; }}>
-            <source src={this.props.source} type="video/mp4" />
+      <div className='player'>
+        <div className='video-container'>
+          <video className='video' ref={(video) => { this.video = video; }}>
+            <source src={this.props.source} type='video/mp4' />
           </video>
+          <div ref={(seeking) => { this.seeking = seeking; }} className='seeking-overlay'></div>
           <PlayPause onClick={this.onPlayPauseClick.bind(this)} playing={this.state.playing} />
         </div>
-        <Seeker onSeek={this.seek.bind(this)} currentPosition={this.state.currentPosition} >
+        <Seeker onSeek={this.seek.bind(this)} currentPosition={this.state.currentPosition} />
+        <div className="hotSpot-container">
           {
             this.props.hotSpots.map((spot, index) => {
               return <HotSpot onClick={this.onHotSpotClick.bind(this)} containerWidth={document.querySelector('.sliders').offsetWidth} spot={spot} duration={this.state.duration} key={index} />
             })
           }
-        </Seeker>
+        </div>
       </div>
     );
   }

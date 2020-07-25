f4b07e3c2629f0d62f796c2e1585460f2418ea3c
Ion D. Filho <ion.drimba@foxbit.com.br>
2020-07-25 16:36:45 -0300

    Fix assets

--- build/precache-manifest.69cc0aa672d073111de962352fcaa7b4.js
+++ build/precache-manifest.69cc0aa672d073111de962352fcaa7b4.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "0c01add353597bb803bf",
-    "url": "/static/css/main.78bbf8de.chunk.css"
+    "url": "static/css/main.78bbf8de.chunk.css"
   },
   {
     "revision": "e8d6013c93bc9ee8919b",
-    "url": "/static/js/2.4eaba9cb.chunk.js"
+    "url": "static/js/2.4eaba9cb.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.4eaba9cb.chunk.js.LICENSE.txt"
+    "url": "static/js/2.4eaba9cb.chunk.js.LICENSE.txt"
   },
   {
     "revision": "0c01add353597bb803bf",
-    "url": "/static/js/main.29e0a82f.chunk.js"
+    "url": "static/js/main.29e0a82f.chunk.js"
   },
   {
     "revision": "c9c2fde0adf59b236e84",
-    "url": "/static/js/runtime-main.b23b03cd.js"
+    "url": "static/js/runtime-main.b23b03cd.js"
   }
 ]);
